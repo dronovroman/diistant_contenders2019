266b96417688d8be01b53e13527101c645d03c0c
dronovroman <romandronov@gmail.com>
2019-09-06 23:15:52 +1000

    init parametrs for API

--- server/cas2029_proto.py
+++ server/cas2029_proto.py
@@ -10,9 +10,11 @@ import pandas as pd
 from flask import Flask
 import urllib.request 
 
+
+PARAMS = {}
+
 app = Flask (__name__) 
-
-
+app.secret_key = 'key'
 
 try:
     with urllib.request.urlopen("https://www.data.act.gov.au/resource/emq2-8bc4.json") as url:
@@ -27,6 +29,14 @@ df['lat']=df['location'].apply(pd.Series)['latitude']
 df['lon']=df['location'].apply(pd. Series)['longitude']
 df['lat']=df['lat'].astype('float')
 df['lon']=df['lon'].astype('float')
+
+
+print('crash data:', len(crash_df))
+PARAMS ['POINT'] = (-35.241951, 149.090256)  # coord
+PARAMS ['RADIUS'] = 2   # radius in km
+PARAMS ['TIME'] = 960   # time in minutes after midnight
+PARAMS ['MARGIN'] = 100  #margin in minutes to calculate range
+
 
 @app.route("/", methods = ['GET'])
 def home():
