ef73eaa5b80e82c7186a189b1b6d7f9ebcb418b8
dronovroman <romandronov@gmail.com>
2019-09-06 23:05:16 +1000

    getting data from ACT pedestrian crashes API..

--- server/cas2029_proto.py
+++ server/cas2029_proto.py
@@ -5,12 +5,23 @@ Created on Fri Sep  6 22:55:01 2019
 @author: roman
 """
 # Server side for CAS2029
-
-
+import json
+import pandas as pd 
 from flask import Flask
-
+import urllib.request 
 
 app = Flask (__name__) 
+
+
+
+try:
+    with urllib.request.urlopen("https://www.data.act.gov.au/resource/emq2-8bc4.json") as url:
+        data = json.loads(url.read().decode())
+    crash_df = pd.DataFrame(data)
+    print('Fetched pedestrian crash data from SODA API')
+except:
+    print('Failed to call pedestrian SODA API...')
+
 
 
 @app.route("/", methods = ['GET'])
